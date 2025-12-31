7b401834d843399c550c9742415ec8a383fa3253
Henrry Rojas <henrryrojas@MacBook-Air-de-Henrry.local>
2025-12-30 21:31:35 -0500

    add url to registro-academico

--- backend/academic-registration-service/src/graduation-certificates/graduation-certificates.controller.ts
+++ backend/academic-registration-service/src/graduation-certificates/graduation-certificates.controller.ts
@@ -17,7 +17,7 @@ import type { ApproveRequestDto } from './dto/approve-request.dto';
 import type { UpdateCertificateDto } from './dto/update-certificate.dto';
 import type { Request, Response } from 'express';
 
-@Controller('academic-registration/api/v1/certificates')
+@Controller('certificates')
 export class GraduationCertificatesController {
   constructor(private readonly service: GraduationCertificatesService) {}
 
