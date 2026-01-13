15806b31ce28c262db2c465891363ca4f36760f1
Henrry Rojas <henrryrojas@MacBook-Air-de-Henrry.local>
2026-01-13 18:47:51 -0500

    enviar correos desde el microservicio de notificaciones

--- backend/notifications-service/src/main.ts
+++ backend/notifications-service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidUnknownValues: false,
+    }),
+  );
+  
+  const port = process.env.PORT || 3009;
+  await app.listen(port);
+  console.log(`Notifications service corriendo en puerto ${port} con CORS habilitado`);
 }
 bootstrap();
