df6d87de1f7b6299baeca4742887b7ce3cd43a63
Julian Rojas <julianrojasasav30@gmail.com>
2026-05-14 09:35:17 -0500

    Errores alertas estilos plataforma.

--- apps/mfe-estructura-org/src/main.tsx
+++ apps/mfe-estructura-org/src/main.tsx
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { EstructuraOrganizacionalModule } from './components/estructura-organizacional/EstructuraOrganizacionalModule'
 import './index.css'
 
-const _cspNonce = document.querySelector<HTMLMetaElement>('meta[name="csp-nonce"]')?.content ?? '';
-if (_cspNonce) {
-  const _origCreate = document.createElement.bind(document);
-  document.createElement = function(tag: string, opts?: ElementCreationOptions) {
-    const el = _origCreate(tag as 'div', opts);
-    if (tag.toLowerCase() === 'style') el.setAttribute('nonce', _cspNonce);
-    return el;
-  } as typeof document.createElement;
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className="p-8 bg-gray-50 min-h-screen">
